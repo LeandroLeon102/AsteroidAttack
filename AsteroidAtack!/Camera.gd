cdfbb80546f7db841a1de594f65890097f3220ba
LeandroLeon102 <leandro.leonsoto@gmail.com>
2022-10-22 01:00:49 -0600

    removing files

--- AsteroidAtack!/Camera.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-
-func zoom_in():
-	$AnimationPlayer.play("zoom_in")
-	
-func zoom_out():
-	$AnimationPlayer.play("zoom_out")
--- HowToPlayMenu/HowToPlayMenu.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-signal MainMenu
-
-
-func _on_MainMenu_pressed():
-	emit_signal("MainMenu")
